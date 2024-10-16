7cfde402d2784f456dc6d446cc7290489382bc1e
Pedro Nelson Nóbrega Monteiro Leite Fonseca <pn77427@gmail.com>
2024-10-16 09:10:37 -0300

    Criação do gráfico de Volume de Postagens por Gênero

--- js/chartRender.js
+++ js/chartRender.js
@@ -1,11 +1,11 @@
-var graficoPostagensTipo = document
+var graficoPostagensRede = document
  .getElementById("grafico-postagens-rede")
  .getContext("2d");
 
-new Chart(graficoPostagensTipo, {
+new Chart(graficoPostagensRede, {
   type: "bar",
   data: {
-    labels: ["Desconstrutivo", "Neutro", "Construtivo"],
+    labels: ["Desconstrutivas", "Neutras", "Construtivas"],
     datasets: [
       {
         label: "Instagram",
@@ -46,7 +46,66 @@ new Chart(graficoPostagensTipo, {
         stacked: true,
         title: {
           display: true,
-          text: "Posts",
+          text: "Postagens",
+          font: {
+            size: 16,
+            weight: 600,
+          },
+        },
+      },
+    },
+  },
+});
+
+var graficoPostagensGenero = document
+ .getElementById("grafico-postagens-genero")
+ .getContext("2d");
+
+ new Chart(graficoPostagensGenero, {
+  type: "bar",
+  data: {
+    labels: ["Desconstrutivas", "Neutras", "Construtivas"],
+    datasets: [
+      {
+        label: "Masculino",
+        backgroundColor: "rgba(23,54,255,0.75)",
+        data: [22, 19, 59],
+      },
+      {
+        label: "N/A",
+        backgroundColor: "rgba(196, 204, 255,0.75)",
+        data: [17, 62, 11],
+      },
+      {
+        label: "Feminino",
+        backgroundColor: "rgba(245,66,152,0.75)",
+        data: [49, 32, 19],
+      }
+    ],
+  },
+  options: {
+    plugins: {
+      title: {
+        display: false,
+      },
+      legend: {
+        display: true,
+      },
+    },
+    scales: {
+      x: {
+        stacked: true,
+        ticks: {
+          font: {
+            size: 16,
+          },
+        },
+      },
+      y: {
+        stacked: true,
+        title: {
+          display: true,
+          text: "Postagens",
           font: {
             size: 16,
             weight: 600,
