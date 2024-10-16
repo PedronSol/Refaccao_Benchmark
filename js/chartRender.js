7114345fe903b11cd48029febf8a7d80ca255f3f
Pedro Nelson Nóbrega Monteiro Leite Fonseca <pn77427@gmail.com>
2024-10-16 09:01:23 -0300

    Criação do gráfico de postagens por rede

--- js/chartRender.js
+++ js/chartRender.js
@@ -1,68 +1,58 @@
-new Chart(document.getElementById("grafico-postagens-dia"), {
-    type: 'line',
-    data: {
-      labels: ['30/09/2024','01/10/2024','02/10/2024','03/10/2024','04/10/2024','05/10/2024','06/10/2024','07/10/2024','08/10/2024','09/10/2024'],
-      datasets: [{ 
-          data: [30,50,25,64,100,10,12,8,5,40],
-          label: "Natália",
-          borderColor: "#de1414",
-          fill: false,
-        }, { 
-          data: [5,91,63,71,14,55,26,30,1,7],
-          label: "Paulinho",
-          borderColor: "#144be3",
-          fill: false,
-        }
-      ]
-    },
-    options: {
+var graficoPostagensTipo = document
+ .getElementById("grafico-postagens-rede")
+ .getContext("2d");
+
+new Chart(graficoPostagensTipo, {
+  type: "bar",
+  data: {
+    labels: ["Desconstrutivo", "Neutro", "Construtivo"],
+    datasets: [
+      {
+        label: "Instagram",
+        backgroundColor: "rgba(193,53,132,0.75)",
+        data: [47, 23, 30],
+      },
+      {
+        label: "Twitter/X",
+        backgroundColor: "rgba(2,161,242,0.75)",
+        data: [72, 9, 19],
+      },
+      {
+        label: "Facebook",
+        backgroundColor: "rgba(66,103,178,0.75)",
+        data: [23, 21, 56],
+      },
+    ],
+  },
+  options: {
+    plugins: {
       title: {
         display: false,
-        text: 'Postagens por dia',
       },
-      responsive: true,
       legend: {
-        position: 'right'
-      }
+        display: false,
+      },
     },
-  });
-
-new Chart(document.getElementById("donut-chart-paulinho"), {
-  type: 'doughnut',
-  data: {
-    labels: ["Positivas", "Negativas", "Neutras"],
-    datasets: [
-      {
-        label: "Postagens",
-        backgroundColor: ["#2dba09", "#ba0909","#737272"],
-        data: [2500, 800, 450]
-      }
-    ]
+    scales: {
+      x: {
+        stacked: true,
+        ticks: {
+          font: {
+            size: 16,
+          },
+        },
+      },
+      y: {
+        stacked: true,
+        title: {
+          display: true,
+          text: "Posts",
+          font: {
+            size: 16,
+            weight: 600,
+          },
+        },
+      },
+    },
   },
-  options: {
-    title: {
-      display: true,
-      text: 'Resumo das postagens (Paulinho)'
-    }
-  }
-});
-
-new Chart(document.getElementById("donut-chart-natalia"), {
-  type: 'doughnut',
-  data: {
-    labels: ["Positivas", "Negativas", "Neutras"],
-    datasets: [
-      {
-        label: "Postagens",
-        backgroundColor: ["#2dba09", "#ba0909","#737272"],
-        data: [1250, 900, 2000]
-      }
-    ]
-  },
-  options: {
-    title: {
-      display: true,
-      text: 'Resumo das postagens (Natalia)'
-    }
-  }
 });
